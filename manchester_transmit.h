e8baec442366da4c37ed40d18edfa774ff8b86c3
Emrah Duatepe <duatepee@gmail.com>
2022-08-21 11:57:30 +0300

    Update manchester_transmit.h

--- manchester_transmit.h
+++ manchester_transmit.h
@@ -21,7 +21,7 @@ typedef enum
 }man_tx_state_t;
 
 void manchester_tx_set_callback(uint8_t b_id, void(*fp)(void));
-void manchester_tx_start_it(uint8_t b_id, uint16_t *aw_data, uint8_t size);
+void manchester_tx_start_it(uint8_t b_id, const uint16_t *aw_data, uint8_t size);
 void manchester_tx_isr(uint8_t b_id);
 uint16_t nrz_to_manchester(uint8_t b_data);
 
