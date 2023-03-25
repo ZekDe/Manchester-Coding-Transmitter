f9ed47c667fd43c4eaa5e02349b0fccacfa84f05
Emrah Duatepe <duatepee@gmail.com>
2023-03-25 22:54:35 +0300

    Update soft_timer.h

--- soft_timer.h
+++ soft_timer.h
@@ -4,15 +4,22 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-#define SOFT_TIMER_OBJ_SIZE	1
-#define TON_OBJ_SIZE	1
+enum
+{
+	TIMER_ID_0 = 0,
+	TIMER_ID_END,
+};
+
+enum
+{
+	TON_ID_0 = 0,
+	TON_ID_END,
+};
 
 void timer_check(uint8_t b_id, uint32_t dw_now);
 void timer_start(uint8_t b_id);
 void timer_stop(uint8_t b_id);
 void timer_set(uint8_t b_id, uint32_t dw_interval, void(*cb)(void));
 bool TON(uint8_t b_id, bool o_in, uint32_t dw_now, uint32_t dw_preset_time);
-bool TON_16U(uint8_t b_id, bool o_in, uint32_t dw_now, uint32_t dw_preset_time);
-
 
 #endif /* SOFT_TIMER_H */
